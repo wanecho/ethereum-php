3db5eeadf9e1e4789f757d95db47809e94f1f0f5
wuheihei <229643466@qq.com>
2021-06-10 22:48:23 +0800

    增加Heco

--- example/account.php
+++ example/account.php
@@ -5,9 +5,14 @@ require_once '../vendor/autoload.php';
 use Ethereum\Eth;
 use Ethereum\InfuraApi;
 use Ethereum\PEMHelper;
-$eth = new Eth(new InfuraApi('d5f9d42ef09845d485ba9520847869e2'));
+use Ethereum\HecoScanApi;
 
-//$pk = PEMHelper::generateNewPrivateKey();
-//var_dump($pk);
-$addr =  PEMHelper::privateKeyToAddress("5d4dc0d9e1df3071448cc1262523cbc02d60edad19beebc03d91a19ca9ca4944");
-var_dump($addr);
+// $eth = new Eth(new InfuraApi('d5f9d42ef09845d485ba9520847869e2'));
+
+// //$pk = PEMHelper::generateNewPrivateKey();
+// //var_dump($pk);
+// $addr =  PEMHelper::privateKeyToAddress("5d4dc0d9e1df3071448cc1262523cbc02d60edad19beebc03d91a19ca9ca4944");
+// var_dump($addr);
+
+$Heco = new Eth(new HecoScanApi('VGE7432V26E4FN5MT81F1GB9KG4PXT7Q3U'));
+echo $Heco->ethBalance('0x9f1082EAc766Bc379791242Aa90e43da86dbdCda');
---
+++ src/HecoScanApi.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * author: NanQi
+ * datetime: 2019/7/3 17:53
+ */
+namespace Ethereum;
+
+class HecoScanApi implements ProxyApi {
+    protected $apiKey;
+    protected $network;
+
+    function __construct(string $apiKey, $network = 'mainnet') {
+        $this->apiKey = $apiKey;
+        $this->network = $network;
+    }
+
+    public function send($method, $params = [])
+    {
+        $defaultParams = [
+            'module' => 'proxy',
+            'tag' => 'latest',
+        ];
+        foreach ($defaultParams as $key => $val) {
+            if (!isset($params[$key])) {
+                $params[$key] = $val;
+            }
+        }
+
+        $preApi = 'api';
+        if ($this->network != 'mainnet') {
+            $preApi .= '-' . $this->network;
+        }
+
+        $url = "https://$preApi.hecoinfo.com/api?action={$method}&apikey={$this->apiKey}";
+        if ($params && count($params) > 0) {
+            $strParams = http_build_query($params);
+            $url .= "&{$strParams}";
+        }
+        $res = Utils::httpRequest('GET', $url);
+        if (isset($res['result'])) {
+            return $res['result'];
+        } else {
+            return $res;
+        }
+    }
+
+    public function gasPrice()
+    {
+        return $this->send('eth_gasPrice');
+    }
+
+    public function ethBalance(string $address)
+    {
+        $params['module'] = 'account';
+        $params['address'] = $address;
+        $retDiv = Utils::fromWei($this->send('balance', $params), 'ether');
+        if (is_array($retDiv)) {
+            return Utils::divideDisplay( (array) $retDiv, 18);
+        } else {
+            return $retDiv;
+        }
+    }
+
+    public function receiptStatus(string $txHash): ?bool
+    {
+        $res = $this->send('eth_getTransactionByHash', ['txhash' => $txHash]);
+        if (!$res) {
+            return false;
+        }
+
+        if (!$res['blockNumber']) {
+            return null;
+        }
+
+        $params['module'] = 'transaction';
+        $params['txhash'] = $txHash;
+
+        $res =  $this->send('gettxreceiptstatus', $params);
+        return $res['status'] == '1';
+    }
+
+    public function getTransactionReceipt(string $txHash)
+    {
+        $res = $this->send('eth_getTransactionReceipt', ['txhash' => $txHash]);
+        return $res;
+    }
+
+    public function sendRawTransaction($raw)
+    {
+        return $this->send('eth_sendRawTransaction', ['hex' => $raw]);
+    }
+
+    public function getNonce(string $address)
+    {
+        return $this->send('eth_getTransactionCount', ['address' => $address]);
+    }
+
+    public function getNetwork(): string
+    {
+        return $this->network;
+    }
+
+    public function ethCall($params): string
+    {
+        return  $this->send('eth_call',$params);
+    }
+
+}
